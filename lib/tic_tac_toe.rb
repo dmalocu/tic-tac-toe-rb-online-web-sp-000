3d12e6806de9bfea368d3b5910f54bd119831d21
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-16 13:41:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,8 +76,10 @@ def won?(board)
       return win_combo 
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
-    end 
-  return false 
+    else 
+      #do nothing 
+    end
+  end
 end 
 
 def full?
