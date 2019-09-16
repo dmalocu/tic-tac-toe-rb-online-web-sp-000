03fbfa5e2fc550f72a6cb4125551fa27425ccda6
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-16 13:47:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,14 +55,6 @@ def turn(board)
   end
 end
 
-def turn_count
-  
-end 
-
-def current_player
-
-end
-
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     if (board[win_combination[0]]) == "X" && (board[win_combination[1]]) == "X" && (board[win_combination[2]]) == "X" 
