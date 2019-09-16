14d75e863bc9d3feb310874c83830aecd298b29e
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-16 13:45:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -113,8 +113,21 @@ def winner(board)
   end
 end
 
-def turn_count
-  loop do 
-    9.times(turn)
-  end 
+def turn_count(board)
+  count = 0
+  board.each do |turn|
+    if turn.downcase == "x" || turn.downcase == "o"
+      count += 1 
+    end
+  end
+  count
+end
+
+def current_player(board)
+   if turn_count(board) % 2 == 0
+      return "X"
+   else 
+      return "O"
+  end
+end
 end 
